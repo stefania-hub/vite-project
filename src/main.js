82f4d8a43379d0b7910e6a8c344117fb080ca86a
lauraM84 <laura.mazza84@gmail.com>
2026-01-13 12:56:16 +0100

    chart

--- src/main.js
+++ src/main.js
@@ -1,7 +1,95 @@
 import "./style.css";
 import { MeteoService } from "./meteo-service.js";
+import { Chart, LineController, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
 
 const service = new MeteoService();
 
-service.getMeteoData();
+service.getMeteoData().then(meteoData => displayMeteo(meteoData));
 
+function displayMeteo(meteoData) {
+
+    const temperaturepoints = getTemperaturePoints(meteoData);
+    const rainpoints = getRainPoints(meteoData);
+    const windpoints = getWindPoints(meteoData);
+
+    testChart("temperature-chart", temperaturepoints);
+    testChart("rain-chart", rainpoints);
+    testChart("wind-chart", windpoints);
+
+    const container = document.getElementById('app');
+    container.innerHTML = "";
+
+    for (const data of meteoData) {
+
+        const card = document.createElement('div');
+
+        const spanTime = document.createElement('span');
+        spanTime.innerHTML = data.time;
+        card.appendChild(spanTime);
+
+        const spanRain = document.createElement('span');
+        spanRain.innerHTML = data.rain;
+        card.appendChild(spanRain);
+
+        const spanTemp = document.createElement('span');
+        spanTemp.innerHTML = data.temperature;
+        card.appendChild(spanTemp);
+
+        const spanCode = document.createElement('span');
+        spanCode.innerHTML = data.code;
+        card.appendChild(spanCode);
+
+        const spanWind = document.createElement('span');
+        spanWind.innerHTML = data.wind;
+        card.appendChild(spanWind);
+
+        container.appendChild(card);
+    }
+
+}
+
+function getTemperaturePoints(meteoData) {
+    return [];
+}
+
+function getRainPoints(meteoData) {
+    return [];
+}
+
+function getWindPoints(meteoData) {
+    return [];
+}
+
+function testChart(canvasId, dataPoints) {
+
+    console.log("data points", dataPoints);
+
+    //[{x:"2026-01-13T00:00", y:9},
+    //{x:"2026-01-13T01:00", y:8},
+    //{x:"2026-01-13T02:00", y:7},...]
+
+    Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement);
+
+    const labels = ["marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre"]
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+    };
+
+    const canvas = document.getElementById(canvasId)
+
+    new Chart(canvas, config)
+
+}
--- src/meteo-service.js
+++ src/meteo-service.js
@@ -5,17 +5,53 @@ export class MeteoService {
     constructor(){}
 
     getMeteoData(){
-        fetch(MeteoService.API_URL)
+       return fetch(MeteoService.API_URL)
         .then(response => response.json())
         .then(data => this.transformData(data))
-        .then(transformedData => console.log(transformedData));
+        .then(transformedData => transformedData);
     }
 
     transformData(data){
 
+        console.log(data);
+
+        const hourlyData = data.hourly;
+
+        // console.log("solo i dati orari", hourlyData);
+
+        const times = hourlyData.time;
+        // console.log("times", times);
+        const temperatures = hourlyData.temperature_2m;
+        // console.log("temp", temperatures);
+        const codes = hourlyData.weather_code;
+        // console.log("codes", codes);
+        const winds = hourlyData.wind_speed_10m;
+        // console.log("winds", winds);
+        const rains = hourlyData.rain;
+        // console.log("rains", rains)
+
+        const newArray = [];
+        for (let i = 0; i < times.length; i++) {
+            const time = times[i];
+            const temperature = temperatures[i];
+            const rain = rains [i];
+            const code = codes [i];
+            const wind = winds [i];
+
+            const hourData = {
+                time: time,
+                temperature: temperature,
+                rain: rain,
+                code: code,
+                wind: wind
+            }
+
+            newArray.push(hourData);
+            
+        }
+        return newArray;
 
 
-        //[{time:"2026-01-17T14:00", temperaure: 14, rain: 3, code: 0, wind: 12},
         // {time:"2026-01-17T15:00", temperaure: 17 rain: 3, code: 0, wind: 12},
         // {time:"2026-01-17T16:00", temperaure: 16, rain: 3, code: 0, wind: 12}]
     }
